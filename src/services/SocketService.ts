f06b06923cc61bf1ea9fb1f814b4e7288efac076
Allwin P G <allwin.george19@gmail.com>
2025-10-30 16:09:45 +0530

    socker server connection

--- src/services/SocketService.ts
+++ src/services/SocketService.ts
@@ -7,7 +7,7 @@ class SocketService {
         console.log("socket service connect function");
         if(!this.socket) {
             console.log(import.meta.env.VITE_BASE_URL);
-            this.socket = io(`https://backend.purplestone-bcfbbc54.centralindia.azurecontainerapps.io/socket`, {
+            this.socket = io(`https://backend.purplestone-bcfbbc54.centralindia.azurecontainerapps.io`, {
                 withCredentials: true,
                 query: {role}
             });
